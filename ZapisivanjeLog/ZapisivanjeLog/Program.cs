1c3e429dfce869b86860efb810a4a0cccccc5eb0
Zoran Hrnčić <zorhrncic@foi.hr>
2017-04-20 22:27:04 +0200

    PRomjena
    promejna
    komentari

--- ZapisivanjeLog/ZapisivanjeLog/Program.cs
+++ ZapisivanjeLog/ZapisivanjeLog/Program.cs
@@ -17,7 +17,7 @@ namespace ZapisivanjeLog
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmIzrcun());
-            MessageBox.Show("Dobar Dan");
+            
 
 
         }
